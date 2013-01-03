8e1e81c526302134f1ad9c7804aa662affd6c240
Stephen Morrison <stephen@quizlet.com>
2013-01-03 12:19:19 -0500

    moving local db config to local runserver

--- runserver.py
+++ runserver.py
@@ -1,2 +1,4 @@
 from smartmarks2 import app
+
+app.config["MONGODB_DB"] = "smartmarks"
 app.run(debug=True)
--- smartmarks2/__init__.py
+++ smartmarks2/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 app = Flask(__name__)
-app.config["MONGODB_DB"] = "smartmarks"
 app.config["SECRET_KEY"] = "(\x9f\x050\xc0\x1f\x0cn\x1eD\xb8\xc9\xfb\xcd]\xb9\x82@\x8d\xa1\xcc\x0fqw"
 
 db = MongoEngine(app)
