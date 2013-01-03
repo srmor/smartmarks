6b1ac47c3cdae744df0b97ce9df29436e6877f3b
Stephen Morrison <stephen@quizlet.com>
2013-01-03 11:42:27 -0500

    changing MongoDB settings for new MongoLab DB"

--- smartmarks2/__init__.py
+++ smartmarks2/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 app = Flask(__name__)
-app.config["MONGODB_DB"] = "smartmarks"
+# app.config["MONGODB_DB"] = "smartmarks"
+app.config["MONGODB_DB"] = "heroku_app10619800"
+app.config["MONGODB_USERNAME"] = "heroku_app10619800"
+app.config["MONGODB_PASSWORD"] = "p4grl2jfla4ncd22724husetgg"
+app.config["MONGODB_HOST"] = "ds047197.mongolab.com"
+app.config["MONGODB_PORT"] = 47197
 app.config["SECRET_KEY"] = "(\x9f\x050\xc0\x1f\x0cn\x1eD\xb8\xc9\xfb\xcd]\xb9\x82@\x8d\xa1\xcc\x0fqw"
 
 db = MongoEngine(app)
