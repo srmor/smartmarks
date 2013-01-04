9d86b680e09f0f17b5934f0a5f995c0fc5c2e0ce
Stephen Morrison <stephen@quizlet.com>
2013-01-03 23:46:30 -0500

    adding sign in api

--- smartmarks2/api_views.py
+++ smartmarks2/api_views.py
@@ -1,7 +1,10 @@
 from smartmarks2 import app
-from flask import request
+from flask import request, Response
 from crossdomain import crossdomain
 from smartmarks2.models import Mark, User
+from flask.ext.bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
 
 
 @app.route('/api/create', methods=['POST'])
@@ -33,3 +36,19 @@ def api_create():
     new_mark.save()
 
     return new_mark.title
+
+
+@app.route('/api/sign-in', methods=['POST'])
+@crossdomain(origin='*')
+def api_sign_in():
+    email = request.form['email']
+    password = request.form['password']
+
+    try:
+        cur_user = User.objects.get(email=email)
+        if bcrypt.check_password_hash(cur_user.password, password):
+            return cur_user.api_key
+        else:
+            return Response('Try Again', status=401, mimetype='application/json')
+    except:
+        return Response('Try Again', status=401, mimetype='application/json')
